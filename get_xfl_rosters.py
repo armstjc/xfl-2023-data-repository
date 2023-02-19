3bb5d22d95a7bfbf8255c2ba0df4425ce61f4849
armstjc <70788681+armstjc@users.noreply.github.com>
2023-02-19 13:40:58 -0500

    Added roster and standings data.

---
+++ get_xfl_rosters.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import json
+from urllib.request import urlopen, urlretrieve
+import requests
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+import time
+
+def get_xfl_rosters(season=2023,week=1,save=False):
+    xfl_api_token = "firefox" ## firefox is a placeholder
+    main_df = pd.DataFrame()
+    row_df = pd.DataFrame()
+
+    ## Yes this is bad practice, but there is nothing in their JSON
+    ## files to indicate what is what.
+    xfl_season = season
+    xfl_week = week
+
+    #headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
+    
+    ## This gets the rosters for all teams, rather than a specific game.
+    url = f"https://api.xfl.com/scoring/v3.30/players?access_token={xfl_api_token}"
+    response = urlopen(url)
+    json_data = json.loads(response.read())
+    
+    for player in json_data:
+        
+        official_id = player['OfficialId']
+        print(f"Player #{official_id}")
+        row_df = pd.DataFrame({'Season':xfl_season,'OfficialID':official_id},index=[0])
+        row_df['JerseyNum'] = player['JerseyNum']
+        row_df['FirstName'] = player['FirstName']
+        row_df['LastName'] = player['LastName']
+        row_df['LastNameSuffix'] = player['LastNameSuffix']
+        row_df['Position'] = player['Position']
+        row_df['PositionLongName'] = player['PositionLongName']
+        row_df['NAbbrev'] = player['NAbbrev']
+        row_df['Height'] = player['Height']
+        row_df['DOB'] = player['DOB']
+        row_df['POB'] = player['POB']
+        row_df['Hometown'] = player['Hometown']
+        row_df['Country'] = player['Country']
+        row_df['CountryCode'] = player['CountryCode']
+        row_df['Nickname'] = player['Nickname']
+        row_df['InjuryStatus'] = player['InjuryStatus']
+        row_df['InjuryDesc'] = player['InjuryDesc']
+        row_df['Headshot'] = player['Headshot']
+        row_df['Initials'] = player['Initials']
+        row_df['TeamId'] = player['TeamId']
+        row_df['Affiliate'] = player['Affiliate']
+        row_df['CloudHeadshotURL'] = player['CloudHeadshotURL']
+        row_df['SquadId'] = player['SquadId']
+        row_df['College'] = player['College']
+        row_df['LeagueStatus'] = player['LeagueStatus']
+
+        main_df = pd.concat([main_df,row_df],ignore_index=True)
+
+    if save == True:
+        main_df.to_csv(f'rosters/{xfl_season}_xfl_roster.csv',index=False)
+        main_df.to_parquet(f'rosters/{xfl_season}_xfl_roster.parquet',index=False)
+
+        main_df['Week'] = xfl_week
+        main_df.to_csv(f'rosters/weekly_rosters/csv/{xfl_season}_{xfl_week}_xfl_roster.csv',index=False)
+        main_df.to_parquet(f'rosters/weekly_rosters/parquet/{xfl_season}_{xfl_week}_xfl_roster.parquet',index=False)
+        #urlretrieve(url, filename=f"rosters/weekly_rosters/json/{xfl_season}_{xfl_week}_xfl_roster.json")
+        with open(f"rosters/weekly_rosters/json/{xfl_season}_{xfl_week}_xfl_roster.json", "w+") as f:
+            f.write(json.dumps(json_data,indent=2))
+
+    main_df['Week'] = xfl_week
+    return main_df
+
+def main():
+    season = 2023
+    week = 2
+    get_xfl_rosters(season,week,True)
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ get_xfl_standings.py
@@ -0,0 +1,133 @@
+import pandas as pd
+import json
+from urllib.request import urlopen, urlretrieve
+import requests
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+import time
+
+def get_xfl_rosters(season=2023,week=1,save=False):
+    xfl_api_token = "firefox" ## firefox is a placeholder
+    main_df = pd.DataFrame()
+    row_df = pd.DataFrame()
+
+    ## Yes this is bad practice, but there is nothing in their JSON
+    ## files to indicate what is what.
+    xfl_season = season
+    xfl_week = week
+
+    #headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
+    
+    ## This gets the rosters for all teams, rather than a specific game.
+    url = f"https://api.xfl.com/scoring/v3.30/standings?access_token={xfl_api_token}"
+    response = urlopen(url)
+    json_data = json.loads(response.read())
+    
+    for player in tqdm(json_data):
+        
+        official_id = player['OfficialId']
+        #print(f"Player #{official_id}")
+        row_df = pd.DataFrame({'Season':xfl_season,'OfficialID':official_id},index=[0])
+        row_df['Rank'] = player['Rank']
+        row_df['RankInConference'] = player['RankInConference']
+        row_df['RankInDivision'] = player['RankInDivision']
+        row_df['GamesPlayed'] = player['GamesPlayed']
+        row_df['GamesBack'] = player['GamesBack']
+        row_df['ScoreDiff'] = player['ScoreDiff']
+        row_df['ScoreFor'] = player['ScoreFor']
+        row_df['ScoreAgainst'] = player['ScoreAgainst']
+        row_df['RankInWildcard'] = player['RankInWildcard']
+        row_df['Streak'] = player['Streak']
+        row_df['Last10'] = player['Last10']
+        row_df['ClinchIndicator'] = player['ClinchIndicator']
+        row_df['ConferenceScoreFor'] = player['ConferenceScoreFor']
+        row_df['ConferenceScoreAgainst'] = player['ConferenceScoreAgainst']
+        row_df['DivisionScoreFor'] = player['DivisionScoreFor']
+        row_df['DivisionScoreAgainst'] = player['DivisionScoreAgainst']
+        row_df['City'] = player['City']
+        row_df['Mascot'] = player['Mascot']
+        row_df['EarnedPoints'] = player['EarnedPoints']
+        row_df['Wins'] = player['Wins']
+        row_df['Losses'] = player['Losses']
+        row_df['Ties'] = player['Ties']
+        row_df['WinPct'] = player['WinPct']
+        row_df['OTWins'] = player['OTWins']
+        row_df['OTLosses'] = player['OTLosses']
+        row_df['OTTies'] = player['OTTies']
+        row_df['ShootoutWins'] = player['ShootoutWins']
+        row_df['ShootoutLosses'] = player['ShootoutLosses']
+        row_df['ConferenceWins'] = player['ConferenceWins']
+        row_df['ConferenceLosses'] = player['ConferenceLosses']
+        row_df['ConferenceTies'] = player['ConferenceTies']
+        row_df['ConferenceWinPct'] = player['ConferenceWinPct']
+        row_df['DivisionWins'] = player['DivisionWins']
+        row_df['DivisionLosses'] = player['DivisionLosses']
+        row_df['DivisionTies'] = player['DivisionTies']
+        row_df['DivisionWinPct'] = str(player['DivisionWinPct'])
+        row_df['RoadEarnedPoints'] = player['RoadEarnedPoints']
+        row_df['RoadWins'] = player['RoadWins']
+        row_df['RoadLosses'] = player['RoadLosses']
+        row_df['RoadTies'] = player['RoadTies']
+        row_df['RoadWinPct'] = player['RoadWinPct']
+        row_df['RoadOTWins'] = player['RoadOTWins']
+        row_df['RoadOTLosses'] = player['RoadOTLosses']
+        row_df['RoadOTTies'] = player['RoadOTTies']
+        row_df['RoadShootoutWins'] = player['RoadShootoutWins']
+        row_df['RoadShootoutLosses'] = player['RoadShootoutLosses']
+        row_df['RoadConferenceWins'] = player['RoadConferenceWins']
+        row_df['RoadConferenceLosses'] = player['RoadConferenceLosses']
+        row_df['RoadConferenceTies'] = player['RoadConferenceTies']
+        row_df['RoadConferenceWinPct'] = player['RoadConferenceWinPct']
+        row_df['RoadDivisionWins'] = player['RoadDivisionWins']
+        row_df['RoadDivisionLosses'] = player['RoadDivisionLosses']
+        row_df['RoadDivisionTies'] = player['RoadDivisionTies']
+        row_df['RoadDivisionWinPct'] = str(player['RoadDivisionWinPct'])
+        row_df['HomeEarnedPoints'] = player['HomeEarnedPoints']
+        row_df['HomeWins'] = player['HomeWins']
+        row_df['HomeLosses'] = player['HomeLosses']
+        row_df['HomeTies'] = player['HomeTies']
+        row_df['HomeWinPct'] = player['HomeWinPct']
+        row_df['HomeOTWins'] = player['HomeOTWins']
+        row_df['HomeOTLosses'] = player['HomeOTLosses']
+        row_df['HomeOTTies'] = player['HomeOTTies']
+        row_df['HomeShootoutWins'] = player['HomeShootoutWins']
+        row_df['HomeShootoutLosses'] = player['HomeShootoutLosses']
+        row_df['HomeConferenceWins'] = player['HomeConferenceWins']
+        row_df['HomeConferenceLosses'] = player['HomeConferenceLosses']
+        row_df['HomeConferenceTies'] = player['HomeConferenceTies']
+        row_df['HomeConferenceWinPct'] = player['HomeConferenceWinPct']
+        row_df['HomeDivisionWins'] = player['HomeDivisionWins']
+        row_df['HomeDivisionLosses'] = player['HomeDivisionLosses']
+        row_df['HomeDivisionTies'] = player['HomeDivisionTies']
+        row_df['HomeDivisionWinPct'] = str(player['HomeDivisionWinPct'])
+        row_df['HockeyOTAndSOLosses'] = player['HockeyOTAndSOLosses']
+        row_df['HockeyRegulationAndOTWins'] = player['HockeyRegulationAndOTWins']
+        row_df['HockeyRoadOTAndSOLosses'] = player['HockeyRoadOTAndSOLosses']
+        row_df['HockeyHomeOTAndSOLosses'] = player['HockeyHomeOTAndSOLosses']
+
+        main_df = pd.concat([main_df,row_df],ignore_index=True)
+
+    if save == True:
+        
+        main_df.to_csv(f'standings/{xfl_season}_xfl_standings.csv',index=False)
+        main_df.to_parquet(f'standings/{xfl_season}_xfl_standings.parquet',index=False)
+
+        main_df['Week'] = xfl_week
+
+        main_df.to_parquet(f'standings/weekly_standings/parquet/{xfl_season}_{xfl_week}_xfl_standings.parquet',index=False)
+        main_df.to_csv(f'standings/weekly_standings/csv/{xfl_season}_{xfl_week}_xfl_standings.csv',index=False)
+        #urlretrieve(url, filename=f"standings/weekly_standingss/json/{xfl_season}_{xfl_week}_xfl_standings.json")
+        with open(f"standings/weekly_standings/json/{xfl_season}_{xfl_week}_xfl_standings.json", "w+") as f:
+            f.write(json.dumps(json_data,indent=2))
+
+    main_df['Week'] = xfl_week
+
+    return main_df
+
+def main():
+    season = 2023
+    week = 2
+    get_xfl_rosters(season,week,True)
+
+if __name__ == "__main__":
+    main()

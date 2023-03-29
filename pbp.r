26e9f5295b1206a8006f7e59274a1b5cf27d9105
Joseph Armstrong <70788681+armstjc@users.noreply.github.com>
2023-03-29 19:56:24 -0400

    Update pbp.r

--- pbp.r
+++ pbp.r
@@ -1,8 +1,12 @@
-#install.packages("nflfastR")
+## Ensure that nflfastR exists in this context.
+install.packages("nflfastR")
+
+## Load nflfastR in this context
 library(nflfastR)
 
+## Apply EP caclulations
 pbp_data <- read.csv("pbp/season/csv/2023_xfl_pbp.csv")
 df <- calculate_expected_points(pbp_data)
 
-
+## Save off the data.
 write.csv(df, "pbp/season/csv/2023_xfl_pbp.csv", row.names = FALSE)

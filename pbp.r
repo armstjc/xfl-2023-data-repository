5a894bf966ba71cfe229b24810e9775384ab6cbe
armstjc <70788681+armstjc@users.noreply.github.com>
2023-03-29 19:09:57 -0400

    3/29/2023 Stats Update

--- pbp.r
+++ pbp.r
@@ -1,7 +1,8 @@
-install.packages("nflfastR")
+#install.packages("nflfastR")
 library(nflfastR)
 
-pbp_data = read.csv('pbp/season/csv/2023_xfl_pbp.csv')
+pbp_data <- read.csv("pbp/season/csv/2023_xfl_pbp.csv")
 df <- calculate_expected_points(pbp_data)
 
-write.csv(df, 'pbp/season/csv/2023_xfl_pbp.csv', row.names = FALSE)
+
+write.csv(df, "pbp/season/csv/2023_xfl_pbp.csv", row.names = FALSE)

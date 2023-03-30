25ad462f719a1871de19ae08939df2b707e02b8f
armstjc <70788681+armstjc@users.noreply.github.com>
2023-03-30 11:50:23 -0400

    Added EPA data

--- pbp.r
+++ pbp.r
@@ -1,12 +1,24 @@
 ## Ensure that nflfastR exists in this context.
 #install.packages("nflfastR")
 
-## Load nflfastR in this context
+## Load required libraries
 library(nflfastR)
+library(dplyr)
 
 ## Apply EP caclulations
 pbp_data <- read.csv("pbp/season/csv/2023_xfl_pbp.csv")
 df <- calculate_expected_points(pbp_data)
 
+
+#df <- transform(df, epa = c(ep[-1], NA))
+df <-
+    df %>%
+    group_by(game_id) %>%
+    dplyr::mutate(epa = lag(ep, n = 1, default = NA))
+df["epa"] <- df["epa"] - df["ep"]
+
 ## Save off the data.
-write.csv(df, "pbp/season/csv/2023_xfl_pbp_EP.csv", row.names = FALSE)
+write.csv(df, "pbp/season/csv/2023_xfl_pbp_EP.csv",
+    na = "",
+    row.names = FALSE
+)

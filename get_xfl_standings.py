c58e26fe3419ecd52f230c359aa3a459b3511311
armstjc <70788681+armstjc@users.noreply.github.com>
2023-04-24 19:42:26 -0400

    4/24/2023 Stats Update

--- get_xfl_standings.py
+++ get_xfl_standings.py
@@ -120,7 +120,7 @@ def get_xfl_standings(season=2023,week=1,save=False):
 
 def main():
     season = 2023
-    week = 10
+    week = 11
     get_xfl_standings(season,week,True)
 
     now = datetime.now()
